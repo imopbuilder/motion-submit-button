87d6b0c15ee78e41acb8b855be061419917ec01d
imopbuilder <opbuilder9@gmail.com>
2024-01-03 07:42:53 +0530

    feat: add footer to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/global/footer';
 import Header from '@/components/global/header';
 import { UserEmailForm } from '@/components/pages/home/client';
 import { Fragment } from 'react';
@@ -8,11 +9,12 @@ export default function Home() {
 			<Header />
 			<main>
 				<section>
-					<div className='max-w-96 mx-auto py-14'>
+					<div className='max-w-96 mx-auto py-14 min-h-hvh'>
 						<UserEmailForm />
 					</div>
 				</section>
 			</main>
+			<Footer />
 		</Fragment>
 	);
 }
