31f636b2e1dcf362c73b63966256a2ebbfadc2b6
imopbuilder <opbuilder9@gmail.com>
2024-01-03 08:46:06 +0530

    styles: change margin property for form description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 					<div className='sm:max-w-96 mx-[4%] sm:mx-auto py-8 sm:py-14 min-h-hvh'>
 						<div>
 							<h2 className='font-medium text-2xl'>Login</h2>
-							<p className='text-muted-foreground text-sm mb-4'>Try to type incorrect password</p>
+							<p className='text-muted-foreground text-sm mt-1 mb-4'>Try to type incorrect password</p>
 							<UserEmailForm />
 						</div>
 					</div>
